0bde1a4a2a42cec68f02ea5daf7920da19a46092
Jan <jandylviosva@gmail.com>
2026-07-07 19:03:37 +0800

    Configure Vite for multiple HTML entry points

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()] });
+import { resolve } from 'path';
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        payment: resolve(__dirname, 'payment.html'),
+      },
+    },
+  },
+});
